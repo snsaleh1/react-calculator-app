02b9276ddc11c6b5a7650d2a6cc817bd5cb05719
snsaleh1 <snsaleh11@gmail.com>
2020-01-03 11:33:11 -0600

    components created, button styles set

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="app">Test
+      <div className="calc-wrapper">
         Test
+      </div>
       </div>
     );
   }
---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+const Button = props => {
+    <div>
+        {props.children}
+    </div>
+}
+
+export default Button;
