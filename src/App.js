aa19bbbdb4c7cff4fc66e6573dbcdd9c1acd4118
snsaleh1 <snsaleh11@gmail.com>
2020-01-06 22:42:09 -0600

    input field added to app no functionality yet

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import { Button } from './components/Button';
 import { Input } from './components/Input';
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: ''
+    }
+  }
   render() {
     return (
       <div className="app">
         <div className="calc-wrapper">
+          <Input input={this.state.input}></Input>
           <div className="row">
             <Button>7</Button>
             <Button>8</Button>
