3bf421b502ec55491d3fd32b11a9fe4b7e73f03e
snsaleh1 <snsaleh11@gmail.com>
2020-01-06 22:40:01 -0600

    input styling set

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Button } from './components/Button';
-
+import { Input } from './components/Input';
 class App extends Component {
   render() {
     return (
