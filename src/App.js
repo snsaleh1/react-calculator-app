dd2cc9e1cf1c8aae13ca200e929c156d86373489
snsaleh1 <snsaleh11@gmail.com>
2020-01-09 10:40:47 -0600

    clear button component added to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Button } from './components/Button';
 import { Input } from './components/Input';
+import { ClearButton } from './components/ClearButton';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -38,6 +39,11 @@ class App extends Component {
             <Button>0</Button>
             <Button>=</Button>
             <Button>-</Button>
+          </div>
+          <div className="row">
+            <ClearButton handleClear={() => this.setState({input: " "})}>
+              Clear 
+            </ClearButton>
           </div>
         </div>
       </div>
